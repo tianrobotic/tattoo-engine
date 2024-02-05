7edfa20a69eb2b381c7ce862190e77d7f3c1c763
daomanlet <rouynxia@gmail.com>
2024-02-04 20:39:15 -0500

    Add daemon shell

--- zodiac-client/start.sh
+++ zodiac-client/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-HOST=159.203.47.139 BACKEND_HOST=159.203.47.139 BACKEND_PORT=5000 npm run dev -- --host 159.203.47.139 &> frontend.log
+HOST=159.203.47.139 BACKEND_HOST=159.203.47.139 BACKEND_PORT=5000 npm run dev -- --host 159.203.47.139 &> frontend.log &
